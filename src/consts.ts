0d363ed1fd1ba677a9a30c6484655f217a2170f2
NotGeri <46295973+NotGeri@users.noreply.github.com>
2023-08-08 21:27:34 +0200

    feat: start creating main layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,18 @@
 export const SITE_TITLE = 'Geri\'s Hideout';
 export const SITE_DESCRIPTION = '';
+export const LINKEDIN = 'https://linkedin.com/in/gergelykun/';
+export const GITHUB = 'https://github.com/notgeri';
+export const DISCORD_NAME = 'geri.dev';
+export const DISCORD_INVITE = 'https://discord.gg/AGqMNEPEhk';
+export const EMAIL = 'me@geri.dev';
+export const JOB = {
+    COMPANY: {
+        NAME: 'Bloom',
+        LINK: 'https://bloom.host'
+    },
+    ROLE: 'Technical Manager',
+}
+export const FAVOURITE_BAND = {
+    NAME: 'Beyond The Black',
+    LINK: 'https://www.youtube.com/channel/UCv8qGsOBKIMaIV8_BQrb6tQ'
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            spacing: {
+                '128': '42rem',
+                '196': '48rem',
+            },
+            fontSize: {
+                clamp: 'clamp(12px, 1vw, 1rem)',
+            },
+        },
+        screens: {
+            'small': '0',
+            'large': '900px'
+        }
+    },
+    plugins: [],
 }
