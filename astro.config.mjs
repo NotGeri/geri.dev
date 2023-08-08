84fe87084235706084c755ec5e93465ebcdf4cea
NotGeri <46295973+NotGeri@users.noreply.github.com>
2023-08-08 21:57:56 +0200

    refactor: use Astro assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://geri.dev',
-    integrations: [mdx(), sitemap(), tailwind()]
+    experimental: {
+        assets: true
+    },
+    integrations: [mdx(), tailwind()]
 });
