63f00e1416fcaf25aeec014de640a35a4c6dadd1
NotGeri <46295973+NotGeri@users.noreply.github.com>
2023-08-15 14:58:05 +0200

    feat: add contact page

--- rollup/fontawesome.js
+++ rollup/fontawesome.js
@@ -9,14 +9,16 @@ import {
     faImage,
     faListCheck,
     faQuestion,
+    faRotateRight,
     faServer,
     faUpRightFromSquare,
     faUser,
+    faUserPlus,
 } from '@fortawesome/free-solid-svg-icons';
 
 import {faDiscord, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
-library.add(faDownload, faUser, faBlog, faServer, faFile, faHandshakeSimple, faDownload, faUpRightFromSquare, faEnvelope, faListCheck, faImage, faQuestion, faArrowUpFromBracket);
+library.add(faDownload, faUser, faBlog, faServer, faFile, faHandshakeSimple, faDownload, faUpRightFromSquare, faEnvelope, faListCheck, faImage, faQuestion, faArrowUpFromBracket, faRotateRight, faUserPlus);
 library.add(faGithub, faDiscord, faLinkedin);
 
 window.addEventListener('DOMContentLoaded', () => {
--- src/consts.ts
+++ src/consts.ts
@@ -1,19 +1,26 @@
 export const SITE_ENDPOINT = 'https://new.geri.dev';
-export const SITE_TITLE = 'Geri\'s Hideout';
-export const SITE_DESCRIPTION = '';
+
 export const LINKEDIN = 'https://linkedin.com/in/gergelykun/';
+
 export const GITHUB = 'https://github.com/notgeri';
-export const DISCORD_NAME = 'geri.dev';
-export const DISCORD_INVITE = 'https://discord.gg/AGqMNEPEhk';
+
+export const DISCORD = {
+    NAME: 'geri.dev',
+    USER_INVITE: 'https://discord.com/users/312630873931513856',
+    INVITE: 'https://discord.gg/AGqMNEPEhk'
+};
+
 export const EMAIL = 'me@geri.dev';
+
 export const JOB = {
     COMPANY: {
         NAME: 'Bloom',
         LINK: 'https://bloom.host'
     },
     ROLE: 'Technical Manager',
-}
+};
+
 export const FAVOURITE_BAND = {
     NAME: 'Beyond The Black',
     LINK: 'https://www.youtube.com/channel/UCv8qGsOBKIMaIV8_BQrb6tQ'
-}
+};
